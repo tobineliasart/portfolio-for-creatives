80ba61e14e2dcc0ca56acc530e898b171a30380a
tobineliasart <tobineckstein@my.capilanou.ca>
2023-11-01 19:41:09 -0700

    typing hero effect

--- js/typing.js
+++ js/typing.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
-    // Alternative animation for example
-    // slideToggle("fast");
-  });
-});
-
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
